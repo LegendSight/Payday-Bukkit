8a358683caf45a160a8435b441a8db0c2f02b52c
Ben C <benclabby@gmail.com>
2013-08-28 21:12:16 -0400

    Updates

--- src/delta/pd/Main.java
+++ src/delta/pd/Main.java
@@ -1,6 +1,10 @@
 package delta.pd;
 
+import java.io.File;
+
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,8 +13,56 @@ import delta.pd.command.PD;
 
 public class Main extends JavaPlugin implements Listener {
 
+	static Main instance;
+	
+	public static Main getInstance() {
+		
+		return instance;
+		
+	}
+	
+    public File configFile;
+    public FileConfiguration Config;
+
+    public File arenaFile;
+    public FileConfiguration Arena;
+
+    public File invFile;
+    public FileConfiguration Inv;
+
+    public File chestFile;
+    public FileConfiguration Chest;
+
+    public File spawnFile;
+    public FileConfiguration Spawns;
+	
 	@Override
 	public void onEnable() {
+		
+		instance = this;
+		
+        configFile = new File(getDataFolder(), "config.yml");
+        arenaFile = new File(getDataFolder(), "arena.yml");
+        invFile = new File(getDataFolder(), "inventorys.yml");
+        chestFile = new File(getDataFolder(), "chests.yml");
+        spawnFile = new File(getDataFolder(), "spawns.yml");
+
+        try {
+
+            ConfigManager.getInstance().firstRun();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        this.Config = new YamlConfiguration();
+        this.Arena = new YamlConfiguration();
+        this.Inv = new YamlConfiguration();
+        this.Chest = new YamlConfiguration();
+        this.Spawns = new YamlConfiguration();
+        ConfigManager.getInstance().loadYamls();
+        ConfigManager.getInstance().saveYamls();
 		
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(this, this);
--- src/delta/pd/command/PD.java
+++ src/delta/pd/command/PD.java
@@ -1,36 +1,62 @@
 package delta.pd.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import delta.pd.Main;
+
 public class PD implements CommandExecutor {
 
-	ChatColor g = ChatColor.DARK_GRAY;
+	ChatColor g = ChatColor.GRAY;
 	ChatColor b = ChatColor.BLUE;
 	ChatColor r = ChatColor.RED;
-	ChatColor u = ChatColor.UNDERLINE;
+	ChatColor u = ChatColor.STRIKETHROUGH;
+	
+	int id;
+	int switchint = 1;
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 
-		Player p = (Player) sender;
+		final Player p = (Player) sender;
 		
-		String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.BLUE + "" + ChatColor.DARK_GRAY + "] ";
+		String prefix = g + "[" + b + "PayDay" + g + "] ";
 		
 		if(label.equalsIgnoreCase("pd") || label.equalsIgnoreCase("payday")) {
 
+			/*
+			 * Begin Args0
+			 */
+			
 			if(args.length == 0) {
 				
-			p.sendMessage(g + "" + u + "-------------------- " + prefix + "--------------------");
+			p.sendMessage(g + "" + u + "--------------------" + ChatColor.GRAY + ChatColor.BOLD + "[ " + b + "PayDay" + g + ChatColor.BOLD + " ]" + u + "--------------------");
 			p.sendMessage(g + "Payday for MinecraftUniverse developed by " + r + "BenCS_ " + g + "and" );
-			p.sendMessage(r + "LegendSight. " + g + "**CURRENTLY IN BETA!!!!**");
+			p.sendMessage(r + "LegendSight. " + g + "**CURRENTLY IN ALPHA!!!!** Report bugs on forums!");
 				
 			}
 			
+			/*
+			 * End Args0
+			 * 
+			 * Begin Args1
+			 * 
+			 */
+			
+			if(args.length == 1) {
+				
+				if(label.equalsIgnoreCase("lobby")) {
+					
+					
+					
+				}
+				
+			}
 			
 		}
 		
